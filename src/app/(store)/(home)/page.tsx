1537674d5564e4dbcb8a60498e0eedf08ae75c39
Adeonir Kohl <adeonir@gmail.com>
2023-11-01 18:46:20 -0300

    Add images grid on home page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,3 +1,42 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Home() {
-  return <h1 className="text-2xl">Hello world!</h1>
+  return (
+    <div className="max-h-215 grid-rows-2 grid grid-cols-3 gap-6">
+      <Link
+        className="group col-span-2 row-span-2 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+        href="/"
+      >
+        <Image
+          alt=""
+          className="transition-transform group-hover:scale-105"
+          height={860}
+          quality={100}
+          src="/assets/moletom-ai-side.png"
+          width={860}
+        />
+      </Link>
+      <Link className="group flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900" href="/">
+        <Image
+          alt=""
+          className="transition-transform group-hover:scale-105"
+          height={430}
+          quality={100}
+          src="/assets/moletom-ia-p-devs.png"
+          width={430}
+        />
+      </Link>
+      <Link className="group flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900" href="/">
+        <Image
+          alt=""
+          className="transition-transform group-hover:scale-105"
+          height={430}
+          quality={100}
+          src="/assets/moletom-never-stop-learning.png"
+          width={430}
+        />
+      </Link>
+    </div>
+  )
 }
