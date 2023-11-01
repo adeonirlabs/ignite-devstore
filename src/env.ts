cca3a1356a21e830f8990ced5d7c4a597c3c1233
Adeonir Kohl <adeonir@gmail.com>
2023-11-01 19:58:50 -0300

    Create env schema validation

---
+++ src/env.ts
@@ -0,0 +1,10 @@
+import { envSchema } from '~/schemas/env'
+
+const parsedEnv = envSchema.safeParse(process.env)
+
+if (!parsedEnv.success) {
+  console.error('Invalid environment variables', parsedEnv.error.flatten().fieldErrors)
+  throw new Error(`Invalid environment variables`)
+}
+
+export const env = parsedEnv.data
