fbe0107856bb9df60a8e48bd131b61640841956e
Adeonir Kohl <adeonir@gmail.com>
2023-11-01 18:45:53 -0300

    Fix tailwind config to extend new settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,15 +3,20 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    fontFamily: {
-      sans: ['var(--font-inter)', 'sans-serif'],
-    },
-    gridTemplateRows: {
-      app: 'min-content max-content',
-    },
-    maxWidth: {
-      '8xl': '90rem',
-      '9xl': '100rem',
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)', 'sans-serif'],
+      },
+      gridTemplateRows: {
+        app: 'min-content max-content',
+      },
+      maxHeight: {
+        215: '54rem',
+      },
+      maxWidth: {
+        '8xl': '90rem',
+        '9xl': '100rem',
+      },
     },
   },
   plugins: [],
