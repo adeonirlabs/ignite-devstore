68c94639d3d7e07b3562e3446b237298c95eefeb
Adeonir Kohl <adeonir@gmail.com>
2023-11-01 08:37:00 -0300

    Add content to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,34 @@
+import { Search, ShoppingBag } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
 import type { ComponentProps } from 'react'
 
 export function Header(props: ComponentProps<'header'>) {
-  return <header {...props}>Header</header>
+  return (
+    <header className="flex items-center justify-between" {...props}>
+      <div className="flex items-center gap-4">
+        <Link className="text-2xl font-extrabold text-zinc-50" href="/">
+          devstore
+        </Link>
+        <form className="flex w-80 items-center gap-3 rounded-full bg-zinc-900 px-4 py-2 ring-2 ring-zinc-700">
+          <Search className="h-5 w-5 text-zinc-500" />
+          <input
+            className="w-full bg-transparent text-sm placeholder:text-zinc-500 focus:outline-none"
+            placeholder="Buscar produtos"
+          />
+        </form>
+      </div>
+      <div className="flex items-center gap-4">
+        <button className="flex items-center gap-2" type="button">
+          <ShoppingBag className="h-5 w-5 text-zinc-500" />
+          <span className="text-sm">Cart (0)</span>
+        </button>
+        <div className="h-6 w-0.5 bg-zinc-700" />
+        <Link className="flex items-center gap-2 hover:underline" href="/">
+          <span className="text-sm">Conta</span>
+          <Image alt="" className="h-6 w-6 rounded-full" height={32} src="https://github.com/adeonir.png" width={32} />
+        </Link>
+      </div>
+    </header>
+  )
 }
