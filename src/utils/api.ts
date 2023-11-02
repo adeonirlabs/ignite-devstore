227b578d06be0493e39ff51adf5658e206a06fe8
Adeonir Kohl <adeonir@gmail.com>
2023-11-01 21:20:07 -0300

    Create fetch api wrapper

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,8 @@ import { env } from '~/env'
 
 export function api(endpoint: string, options?: RequestInit) {
   const baseUrl = env.NEXT_PUBLIC_API_URL
-  const url = new URL(endpoint, baseUrl)
+  const prefix = 'api'
+  const url = new URL(`${prefix}/${endpoint}`, baseUrl)
 
   return fetch(url, options)
 }
