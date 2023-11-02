57694f093f568a94ba6d4d534e1da296df45cd2e
Adeonir Kohl <adeonir@gmail.com>
2023-11-02 07:08:28 -0300

    Fix lint errors

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '~/components/skeleton'
 
 export default function Loading() {
   return (
-    <div className="2xl:h-215 grid max-h-215 grid-cols-3 grid-rows-2 gap-6">
+    <div className="grid max-h-215 grid-cols-3 grid-rows-2 gap-6 2xl:h-215">
       <Skeleton className="col-span-2 row-span-2 aspect-square h-full rounded-lg 2xl:aspect-auto" />
       <Skeleton className="aspect-square rounded-lg 2xl:aspect-auto" />
       <Skeleton className="aspect-square rounded-lg 2xl:aspect-auto" />
