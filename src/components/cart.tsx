657d40405f731f30c2d152b988edda6f39d05d0d
Adeonir Kohl <adeonir@gmail.com>
2023-11-04 06:33:33 -0300

    Add counter badge to the cart button

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -8,9 +8,20 @@ export function Cart() {
   const items = useStore((state) => state.items)
 
   return (
-    <button className="flex items-center gap-2" type="button">
-      <ShoppingBag className="h-5 w-5 text-zinc-500" />
-      <span className="text-sm">Cart ({items.length || 0})</span>
+    <button
+      className="flex items-center gap-3"
+      onClick={() => {
+        console.info({ items })
+      }}
+      type="button"
+    >
+      <div className="relative">
+        <div className="absolute -right-2 -top-2 flex h-5 w-5 items-center justify-center rounded-full bg-teal-500 text-xs">
+          {items.length}
+        </div>
+        <ShoppingBag className="h-6 w-6 text-zinc-500" />
+      </div>
+      <span className="text-sm">Cart</span>
     </button>
   )
 }
