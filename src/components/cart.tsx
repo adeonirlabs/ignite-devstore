d68f425e1873e33ff123b7bb9039d610f2d8d99d
Adeonir Kohl <adeonir@gmail.com>
2023-11-10 20:45:04 -0300

    Update cart component to have count text

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -16,12 +16,17 @@ export function Cart() {
       type="button"
     >
       <div className="relative">
-        <div className="absolute -right-2 -top-2 flex h-5 w-5 items-center justify-center rounded-full bg-teal-500 text-xs">
+        <div
+          aria-hidden
+          className="absolute -right-2 -top-2 flex h-5 w-5 items-center justify-center rounded-full bg-teal-500 text-xs"
+        >
           {items.length}
         </div>
         <ShoppingBag className="h-6 w-6 text-zinc-500" />
       </div>
-      <span className="text-sm">Cart</span>
+      <span className="text-sm">
+        Cart <span className="hidden">{items.length}</span>
+      </span>
     </button>
   )
 }
