27d4bca50388ab955a44173d67bc04f9f4ce255f
Adeonir Kohl <adeonir@gmail.com>
2023-11-10 20:45:48 -0300

    Add search e2e test

---
+++ cypress/e2e/search.cy.ts
@@ -0,0 +1,18 @@
+describe('search product', () => {
+  it('should search for product', () => {
+    cy.visit('/')
+    cy.get('input[name="q"]').type('moletom').parent().submit()
+
+    cy.location('pathname').should('eq', '/search')
+    cy.location('search').should('eq', '?q=moletom')
+
+    cy.get('a[href^="/product"]').should('exist')
+  })
+
+  it('should redirect to home page if search query is empty', () => {
+    cy.on('uncaught:exception', () => false)
+    cy.visit('/search?q=')
+
+    cy.location('pathname').should('eq', '/')
+  })
+})
