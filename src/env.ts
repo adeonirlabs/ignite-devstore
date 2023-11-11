f1ed25a88b3c18e5b5b25fa6b6352c894d2da347
Adeonir Kohl <adeonir@gmail.com>
2023-11-11 08:38:34 -0300

    Split env file into server and client

--- src/env.ts
+++ src/env.ts
@@ -1,10 +1,15 @@
-import { envSchema } from '~/schemas/env'
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
 
-const parsedEnv = envSchema.safeParse(process.env)
-
-if (!parsedEnv.success) {
-  console.error('Invalid environment variables', parsedEnv.error.flatten().fieldErrors)
-  throw new Error(`Invalid environment variables`)
-}
-
-export const env = parsedEnv.data
+export const env = createEnv({
+  server: {
+    APP_URL: z.string().url(),
+  },
+  client: {
+    NEXT_PUBLIC_API_URL: z.string().url(),
+  },
+  runtimeEnv: {
+    APP_URL: process.env.APP_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+})
--- src/schemas/env.ts
+++
@@ -1,5 +0,0 @@
-import { z } from 'zod'
-
-export const envSchema = z.object({
-  NEXT_PUBLIC_API_URL: z.string().url(),
-})
